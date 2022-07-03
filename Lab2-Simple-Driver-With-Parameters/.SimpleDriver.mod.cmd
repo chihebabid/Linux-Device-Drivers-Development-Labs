5f3fcfb6579a009b52a71a21d1f79517c6102553
chihebabid <chiheb.abid@fst.utm.tn>
2022-07-03 16:51:34 +0100

    Deleted  :        Lab2-Simple-Driver-With-Parameters/.Module.symvers.cmd 	Deleted :        Lab2-Simple-Driver-With-Parameters/.SimpleDriver.ko.cmd 	Deleted :        Lab2-Simple-Driver-With-Parameters/.SimpleDriver.mod.cmd 	Deleted :        Lab2-Simple-Driver-With-Parameters/.SimpleDriver.mod.o.cmd 	Deleted :        Lab2-Simple-Driver-With-Parameters/.SimpleDriver.o.cmd 	Deleted :        Lab2-Simple-Driver-With-Parameters/.modules.order.cmd

--- Lab2-Simple-Driver-With-Parameters/.SimpleDriver.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chiheb/temp/Linux-Device-Drivers-Development-Labs/Lab2-Simple-Driver-With-Parameters/SimpleDriver.mod := { echo  /home/chiheb/temp/Linux-Device-Drivers-Development-Labs/Lab2-Simple-Driver-With-Parameters/SimpleDriver.o;  echo; } > /home/chiheb/temp/Linux-Device-Drivers-Development-Labs/Lab2-Simple-Driver-With-Parameters/SimpleDriver.mod
